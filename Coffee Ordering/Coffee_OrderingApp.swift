400901ffa3065a81ad57782de5ab495fb2dd3ac7
Hanif Ramadhan <hramadhaan20@gmail.com>
2021-07-15 20:15:54 +0700

    Initial Commit

---
+++ Coffee Ordering/Coffee_OrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_OrderingApp.swift
+//  Coffee Ordering
+//
+//  Created by Hanif Ramadhan Abdillah on 15/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_OrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
